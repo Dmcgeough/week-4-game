ff7e95eb21688f2827b81c3ed432c3a5aefc810f
Dmcgeough <danny.mcgeough@gmail.com>
2017-06-24 00:03:08 -0400

    yeah

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
   
-var characters = {
+var characterList = {
 	"name": ["Reaper", "Soldier 76", "McCree", "Tracer"],
 	"health": [120, 110, 105, 100],
 	"attack": [8, 6, 12, 12],
@@ -18,60 +18,89 @@ var characters = {
 
 
   //initialise variables
-  var Reaper = {
+var characters={
+    Reaper : {
   	"name": "Reaper",
   	"health": 120,
   	"attack": 8,
   	"counter": 8,
   	attackBuff(){
-  		return this.attack += this.attack;
+  		return this.attack += 6;
   	
   	},
 	"isOpponent": false,
 	"isCharacter": false,
-	"characterLock": false,};
+	"characterLock": false,},
 
 
-  var Soldier76 = {
+    Soldier76 : {
   	"name": "Soldier76",
   	"health": 110,
   	"attack": 6,
   	"counter": 10,
   	attackBuff(){
-  		return this.attack += this.attack;
+  		return this.attack += 6;
  	},
  	"isOpponent": false,
 	"isCharacter": false,
-	"characterLock": false,};
+	"characterLock": false,},
 
-  var McCree = {
+    McCree : {
   	"name": "McCree",
   	"health": 105,
   	"attack": 12,
   	"counter": 9,
   	attackBuff(){
-  		return this.attack += this.attack;
+  		return this.attack += 6;
  	},
  	"isOpponent": false,
 	"isCharacter": false,
-	"characterLock": false,};
+	"characterLock": false,},
 
-  var Tracer = {
+    Tracer : {
   	"name": "Tracer",
 	"health": 100,
 	"attack": 12,
 	"counter": 8,
 	attackBuff(){
-  		return this.attack += this.attack;
+  		return this.attack += 6;
 	},
 	"isOpponent": false,
 	"isCharacter": false,
 	"characterLock": false
-	}; 
+	}}; 
 
 	
-	$(".ReaperHP").html(Reaper.health);
-	$(".Soldier76HP").html(Soldier76.health);
-	$(".McCreeHP").html(McCree.health);
-	$(".TracerHP").html(Tracer.health);
+	$(".ReaperHP").html(characters.Reaper.health);
+	$(".Soldier76HP").html(characters.Soldier76.health);
+	$(".McCreeHP").html(characters.McCree.health);
+	$(".TracerHP").html(characters.Tracer.health);
 	
+
+	var attackerchosen=false
+
+	$(".choice").click(function(){
+			if (attackerchosen === false) {
+  $(".attacker").append($(this))
+  attackerchosen = true;
+} else {
+  $(".opponent").append($(this))
+  attackerchosen = false;
+}
+	});
+
+	$(".Attack").click(function(){
+		var attackerName=$(".attacker").find(".choice").data("name")
+		var opponentName=$(".opponent").find(".choice").data("name")
+		console.log(attackerName, opponentName)
+		characters[opponentName].health=characters[opponentName].health-characters[attackerName].attack
+		characters[attackerName].health=characters[attackerName].health-characters[opponentName].counter
+		characters[attackerName].attack= characters[attackerName].attack+6
+		console.log(characters[attackerName].health,characters[opponentName].health)
+	});
+
+	
+
+
+
+
